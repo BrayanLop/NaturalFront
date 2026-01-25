5fef0189b67c3514d188b8dd0d2c735552ce5764
Brayan Lopera <lopera787@gmail.com>
2026-01-25 14:45:57 -0500

    se agrega comentario del servidor de respaldo

--- app/api/api.ts
+++ app/api/api.ts
@@ -5,6 +5,7 @@ import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'ax
 
 const API_URL = 'https://naturalback.vip/api';
 //const API_URL = 'https://localhost:7049/api';
+//const API_URL = 'http://45.236.128.205:5000/api';
 
 
 export const api = axios.create({

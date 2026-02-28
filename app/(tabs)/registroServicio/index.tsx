8eca974a577bbed5109194e44f59cebe90beeb51
Brayan Lopera <lopera787@gmail.com>
2026-02-28 14:09:39 -0500

    se agrega evidencia en el registro servicios

--- app/(tabs)/registroServicio/index.tsx
+++ app/(tabs)/registroServicio/index.tsx
@@ -8,21 +8,20 @@ import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { useFocusEffect, useRouter } from 'expo-router';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import {
-  ActivityIndicator,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Modal,
-  Image,
-  Dimensions,
-  Linking
+    ActivityIndicator,
+    FlatList,
+    Image,
+    Linking,
+    Modal,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
 } from 'react-native';
 import { api } from '../../api/api';
 import { Persona } from '../../api/modelos/persona';
-import { RegistroServicio, Evidencia } from '../../api/modelos/registroServicio';
+import { Evidencia, RegistroServicio } from '../../api/modelos/registroServicio';
 
 export default function ListaRegistros() {
   const router = useRouter();
